c9812b7309ab376fe091364731bce3e5fde52de8
Oreoluwa <ore.craftman@gmail.com>
2025-10-31 10:41:20 +0100

    chore: updated primary menu

--- app/components/app-layout/util.ts
+++ app/components/app-layout/util.ts
@@ -7,10 +7,10 @@ export const navigationItems = [
   //   title: "Features",
   //   link: "#features",
   // },
-  {
-    title: "Transcription",
-    link: "/transcription",
-  },
+  // {
+  //   title: "Transcription",
+  //   link: "/transcription",
+  // },
   {
     title: "How It Works",
     link: "#how-it-works",
